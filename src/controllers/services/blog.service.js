22b20b30efe254ac8621df428e5e15491be77603
mczionjohnson <mczionjohnson@gmail.com>
2024-06-21 05:16:28 +0100

    removed caching due to pagination

--- src/controllers/services/blog.service.js
+++ src/controllers/services/blog.service.js
@@ -1,5 +1,5 @@
 import Blog from "../../models/blogSchema.js";
-import redisClient from "../../integrations/redis.js";
+
 
 import logger from "../../logger/logger.js";
 
@@ -32,28 +32,14 @@ export const getAllBlogs = async (page = 1, limit = 20, query) => {
       return { data: searchData, meta: { page, limit } };
     } else {
   
-      // // set cacheKey and check for cache
-      // const cacheKey = "publishedBlog";
 
-      // // get data from database
-      // const value = await redisClient.get(cacheKey);
-
-      // // check for cache miss
-      // if (value != null) {
-      //   console.log("returning data from cache");
-      //   return { data: JSON.parse(value), meta: { page, limit } };
-      // }
-
-    // cache miss is true, get data from DB
-      // console.log("getting data from DB");
       let publishedBlogs = await Blog.find({ state: "published" })
         .sort({ readCount: -1, readingTime: 1, timestamps: -1 })
         .skip(skip)
         .limit(limit);
 
 
-      // // set cache with expirition of 1 minute
-      // await redisClient.setEx(cacheKey, 1 * 60, JSON.stringify(publishedBlogs));
+  
       return { data: publishedBlogs, meta: { page, limit } };
     }
   } catch (error) {
